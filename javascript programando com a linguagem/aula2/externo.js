23a1247c9117d6aaa480c48d9b6f3a600eed7481
lucasdev <blesslucas89@gmail.com>
2024-03-20 14:17:06 -0500

    aula2 e aula3

---
+++ javascript programando com a linguagem/aula2/externo.js
@@ -0,0 +1,23 @@
+window.onload = function()
+            {
+                carrega_nome();
+            window.document.getElementById('um').onclick = function()
+    
+            {
+                information();
+            }
+            window.document.getElementByld('um').oncliack = function()
+            {
+                information();
+            }
+}
+
+            function information(){
+                var name = window.prompt('digite seu nome');
+                var idade = window.prompt('digite sua idade');
+                window.alert(nome + ' ' + idade);
+            }
+            function carrega_nome()
+            {
+                window.document.getElementByld('nome').value = "Alexandre";
+            }
---
+++ javascript programando com a linguagem/aula3/javascript_aula3_way.js
@@ -0,0 +1,36 @@
+//regras de declaração
+//case sensitive, variaveis e tipos de dados
+nome = 'impacta';
+Nome = 'tecnologia';
+var idade = 29;
+console.log(nome);
+console.log(Nome);
+
+//conversão de dados
+var idade = "30";
+var salario = "123.45";
+
+var soma = idade + salario;
+
+console.log(typeof soma);
+console.log(soma);
+
+
+var soma =  idade + Number(salario);
+console.log(typeof soma);
+console.log(soma);
+
+//tipos especiais
+
+var nome = '';
+var escola = 'impacta';
+var idade = 1.7976931348623157  + 309;
+var resultado = escola * 3
+var curso = null
+
+console.log(nome);
+console.log(resultado); // e para gerar o not a number NaN
+console.log(curso);
+console.log(idade);
+
+
